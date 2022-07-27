034452573debf714077c78ecc220f9de10c048e6
KsanaSal <oasalivon@gmail.com>
2022-07-27 22:29:15 +0300

    Solution of part of the task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const galleryList = galleryItems
+    .map(
+        ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
 console.log(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", galleryList);
