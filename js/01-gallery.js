366346c8cc993dc059c5788c73307bb566b02165
KsanaSal <oasalivon@gmail.com>
2022-07-30 22:53:09 +0300

    Solution tasks 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,16 +4,34 @@ const gallery = document.querySelector(".gallery");
 const galleryList = galleryItems
     .map(
         ({ preview, original, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
     )
     .join("");
-console.log(galleryItems);
 gallery.insertAdjacentHTML("afterbegin", galleryList);
+
+gallery.addEventListener("click", imageClick);
+function imageClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+    document.addEventListener("keydown", (evt) => {
+        if (evt.key === "Escape") {
+            if (instance.visible()) {
+                instance.close();
+            }
+        }
+    });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryList = galleryItems
+    .map(
+        ({ preview, original, description }) => `<li>
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryList);
 
-console.log(galleryItems);
+let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
